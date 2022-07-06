da8a31ba0f3818372f7978d2f2351780ecceac5f
Everton <cabralclarinetista@gmail.com>
2022-07-06 09:23:14 -0300

    realizando correção de identação

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,24 +1,22 @@
 <template>
 <div id="app">
     <main>
-          <section class="container section section_height" id="home">
+      <section class="container section section_height" id="home">
         <h2 class="section_title">Home</h2>
-     </section>
-       <section class="container section section_height" id="about">
-                <h2 class="section_title">About</h2>
-            </section>
+      </section>
+      <section class="container section section_height" id="about">
+        <h2 class="section_title">About</h2>
+      </section>
       <section class="container section section_height" id="skills">
         <h2 class="section_title">Skills</h2>
-     </section>
+      </section>
       <section class="container section section_height" id="portfolio">
-                <h2 class="section_title">Portfolio</h2>
-            </section>
-             <section class="container section section_height" id="contactme">
-                <h2 class="section_title">Contactme</h2>
-            </section>
-
-    </main>
-   
+        <h2 class="section_title">Portfolio</h2>
+      </section>
+      <section class="container section section_height" id="contactme">
+          <h2 class="section_title">Contactme</h2>
+       </section>
+    </main> 
 </div>
    
    
